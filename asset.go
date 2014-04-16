6dc8759e71deb8709dfee5a2a932cc455f816914
Mechiel Lukkien <mechiel@ueber.net>
2014-04-16 17:01:39 +0200

    clarify docs a bit

--- asset.go
+++ asset.go
@@ -1,9 +1,9 @@
 /*
-Package asset is a simple way to embed arbitrary files in your go binary.
-All you have to do is append a zip file to your binary.  Typically on
-launch of your program, you call asset.Fs() to get a handle to the
-vfs.FileSystem that represents the appended zip file.  It is on this vfs
-that you should call Open and friends, as opposed to the normal os.Open.
+Package asset lets you embed files in your go binary by simply appending a zip file to said binary.
+Typically on launch of your program, you call asset.Fs() to get a
+handle to the vfs.FileSystem that represents the appended zip file.
+It is on this vfs that you should call Open and friends, as opposed to
+the normal os.Open.
 
 See http://godoc.org/code.google.com/p/go.tools/godoc/vfs for vfs.FileSystem.
 
@@ -11,49 +11,58 @@ An example:
 
 	// mybinary.go
 
+	import (
+		"code.google.com/p/go.tools/godoc/vfs" // optional, for vfs.OS
+		"bitbucket.org/mjl/asset"
+	)
+
 	func main() {
 		// the error-check is optional, asset.Fs() always returns a non-nil vfs.FileSystem.
 		// however, after failed initialization (eg no zip file was appended to the binary),
-		// all vfs operations return an error.
+		// vfs operations return an error.
 		fs := asset.Fs()
 		if err := asset.Error(); err != nil {
 			log.Fatal(err)
+			// or alternatively fallback to to local file system:
+			// fs = vfs.OS(".")
 		}
 
-		// note that paths passed to fs.Open must be absolute, i.e. starting with a slash.
+		// note: paths must be absolute, i.e. starting with a slash.
 		f, err := fs.Open("/test.txt")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer f.Close()
 		io.Copy(os.Stdout, f)
-
-		// vfs (inside the godoc package) has a convenient function OS(root).
-		// It returns a vfs.FileSystem that reads from the local filesystem.
-		// try:  fs = vfs.OS(".")
 	}
 
 Build your program, let's say the result is "mybinary".
-Now create a zip file, eg on unix: zip -r0 mybinary.zip test.txt.
-Append it to the binary:  cat mybinary.zip >>mybinary.
-If you run mybinrary, it will print the contents of file "test.txt"
+Now create a zip file, eg on unix:
+
+	zip -r0 mybinary.zip test.txt.
+
+Append it to the binary:
+
+	cat mybinary.zip >>mybinary.
+
+If you run mybinary, it will print the contents of file "test.txt"
 in the zip file that you appended to the binary.
 
 To make this work, an assumption about zip files is made:
-That the central directory (with a list of files inside the zip file) is
-contiguous and comes right before the "end of central directory" marker.
-This is almost always the case with zip files.  With this assumption,
-asset can locate the start and end of the zip file that is appended to
-the binary, which archive/zip needs in order to parse the zip file.
+That the central directory (with a list of files inside the zip file)
+comes right before the "end of central directory" marker.  This is almost
+always the case with zip files.  With this assumption, asset can locate
+the start and end of the zip file that is appended to the binary, which
+archive/zip needs in order to parse the zip file.
 
 Some existing tools for reading zip files can still read the
 binary-with-zipfile as a zip file.  For example 7z, and the unzip
 command-line tool.  Windows XP's explorer zip opener does NOT seem to
 understand it, and also Mac OS X's archive utility gets confused.
 
-This has been tested with binaries on Linux (Ubuntu 12.04 LTS), Mac OS X
-(10.9.2) and Windows 8.1.  It appears that these operating systems don't
-mind extra data at the end of the binary.
+This has been tested with binaries on Linux (Ubuntu 12.04), Mac OS X
+(10.9.2) and Windows 8.1.  These operating systems don't
+seem to mind extra data at the end of the binary.
 */
 package asset
 
@@ -90,7 +99,7 @@ func Fs() vfs.FileSystem {
 
 // find end-of-directory struct, near the end of the file.
 // it specifies the size & offset of the central directory.
-// we assume the central directory is contiguous, just before the end-of-central-directory.
+// we assume the central directory is located just before the end-of-central-directory.
 // so that allows us to calculate the original size of the zip file.
 // which in turn allows us to use godoc's zipfs to serve the zip file withend.
 func open() (vfs.FileSystem, error) {
